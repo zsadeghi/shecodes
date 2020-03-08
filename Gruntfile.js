1abcef62c059956c288e5e2b73505d11490dad68
Zohreh Sadeghi <zohreh.sadeghi0@gmail.com>
2020-03-07 18:24:54 -0800

    Set up grunt and npm

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+let libs = {
+    'bootstrap/dist': 'bootstrap'
+};
+
+const fs = require('fs');
+const path = require('path');
+
+function copy(source, destination) {
+    let sourceStat = fs.statSync(source);
+    if (sourceStat.isFile()) {
+        fs.copyFileSync(source, destination);
+    } else if (sourceStat.isDirectory()) {
+        if (!fs.existsSync(destination)) {
+            fs.mkdirSync(destination);
+        } else if (!fs.statSync(destination).isDirectory()) {
+            throw new Error("What are you doing? " + destination + " is not a directory.");
+        }
+        fs.readdirSync(source).forEach((item) => {
+            copy(path.join(source, item), path.join(destination, item));
+        });
+    } else {
+        throw new Error("This is unprecedented. Panicking.");
+    }
+}
+
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+    });
+
+    grunt.registerTask('vendor-files', () => {
+        let vendor = path.join(__dirname, 'vendor');
+        if (!fs.existsSync(vendor)) {
+            fs.mkdirSync(vendor);
+        } else if (!fs.statSync(vendor).isDirectory()) {
+            throw new Error("What are you doing? " + destination + " is not a directory.");
+        }
+        Object.keys(libs).forEach((source) => {
+            // Platform-agnostic path resolution.
+            let sourcePath = path.join(__dirname, 'node_modules', source.replace(/\//g, path.sep));
+            let destinationPath = path.join(__dirname, 'vendor', libs[source].replace(/\//g, path.sep));
+            copy(sourcePath, destinationPath);
+        });
+    });
+
+    // Default task(s).
+    grunt.registerTask('default', ['vendor-files']);
+
+};
